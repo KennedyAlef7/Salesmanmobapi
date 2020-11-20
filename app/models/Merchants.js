c9806f9e94d09eb3965096b3f5c358b11b139a74
Kennedy Alef de Oliveira <kennedy.oliveira@paxbr.com.br>
2020-11-20 17:31:57 -0300

    add model pattterns

--- app/models/Merchants.js
+++ app/models/Merchants.js
@@ -1,14 +1,25 @@
 var Parameter = require ('../../repository/ParametersBuild/MerchantsData');
 const { check, validationResult } = require('express-validator');
+const { json } = require('express');
+
+class Merchant{
+
+    constructor(name, document , id , created_at){
+        this.merchantname = name;
+        this.document = document;
+        this.insidecode = id;
+        this.register_date = created_at
+    }
+}
 
 
 async function getMerchants (merchantIdList, salescode){
 
 
     var result = await Parameter.FindMerchants(merchantIdList, salescode);
-    return result;
-        
+    return ReturnToMerchant(result);
     
+        
 }
 
 function IsValid(req, res, next){
@@ -23,9 +34,31 @@ function IsValid(req, res, next){
     
 }
 
+function ReturnToMerchant (result){
+    var merchantarray = []; 
+    if(Array.isArray(result)){
+
+        result.forEach(element => {
+            merchantarray.push(new Merchant (
+                element.NAME,
+                element.DOCUMENT,
+                element.COD_MERCHANT,
+                element.CREATED_AT
+
+            ))
+            console.log(element);
+        });
+
+        return merchantarray;
+     }
+
+}
+
+
 module.exports = {
     getMerchants: getMerchants,
-    IsValid : IsValid
+    IsValid : IsValid,
+    Merchant: Merchant
 }
 
 
